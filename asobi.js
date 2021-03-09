1bb162d38686f504ebd33bb669bed505bc43d275
kasumi-kaito <79437914+kasumi-kaito@users.noreply.github.com>
2021-03-09 14:57:38 +0900

    Create asobi.js

---
+++ asobi.js
@@ -0,0 +1,10 @@
+function jump(){
+    const pp = document.getElementById('pp')
+    if (pp.value === '名駅'){
+        location.href ="https://kasumi-kaito.github.io/assessment/assessment.html"
+    }
+ else {
+    document.getElementById('hazu').innerHTML = "パスワードが間違っています";
+}}
+
+
