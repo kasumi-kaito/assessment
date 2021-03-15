a30ebe3e6f0ffc696aa7f3b3133161144b1ac8e0
kasumi-kaito <79437914+kasumi-kaito@users.noreply.github.com>
2021-03-15 15:18:51 +0900

    Update asobi.js

--- asobi.js
+++ asobi.js
@@ -5,12 +5,35 @@ function jump(){
     }
     else if(pp.value ===''){
         document.getElementById('hazu').innerHTML = "パスワードが入力されていません"
+        if (hazu.className == null || hazu.className=="") {
+            hazu.className = "active";
+          } else {
+            hazu.className = "";
+          }
+    }
+    else if(pp.value ==='藤岡'){
+        location.href ="https://www.google.com/search?q=n%E9%AB%98&oq=&aqs=chrome.0.69i59i450l8.575409440j0j15&sourceid=chrome&ie=UTF-8"
     }
  else {
     document.getElementById('hazu').innerHTML = "パスワードが間違っています";
+    if (hazu.className == null || hazu.className=="") {
+        hazu.className = "active";
+      } else {
+        hazu.className = "";
+      }
+    }
+    
 }
-}
-const div = document.querySelector('div')
-div.animate({
+const a = document.querySelector('div')
+a.animate({
   opacity: [0, 1]
 }, 1500)
+
+function c() {
+    if (hazu.className == null || hazu.className=="") {
+      hazu.className = "active";
+    } else {
+      hazu.className = "";
+    }
+  }
+
